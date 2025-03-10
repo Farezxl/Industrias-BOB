d984d73f17699e41729221eefba9c1276dd5f544
Farezxl <contato.faresxl@gmail.com>
2025-03-10 10:55:23 -0300

    dev

---
+++ apibob/src/main/java/com/bob/apibob/ApibobApplication.java
@@ -0,0 +1,13 @@
+package com.bob.apibob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibobApplication.class, args);
+	}
+
+}
